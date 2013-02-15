25d891b97efaef857a0fed69d3a8e677298cfc53
wangzhenhuaCode <wangzhenhua.mail@gmail.com>
2013-02-15 15:19:38 -0500

    hjhjhjj

--- WebService/src/ajaxProxy/ProxyServlet.java
+++ WebService/src/ajaxProxy/ProxyServlet.java
@@ -1,11 +1,14 @@
 package ajaxProxy;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.URLEncoder;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
@@ -15,24 +18,22 @@ public class ProxyServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request ,HttpServletResponse response) throws ServletException,IOException
     {
 		String url=request.getParameter("url");
-		url.replace("_#", "?");
-		url.replace("@#", "&");
+		//url.replace("_#", "?");
+		//url.replace("@#", "&");
 		response.setHeader("Cache-Control", "no-cache");
 		
 		URL urlObj = new URL(url);
 		URLConnection urlConn = urlObj.openConnection();
 		urlConn.connect();
-		InputStream in=urlConn.getInputStream();
+		urlConn.setReadTimeout(5000);
+		BufferedReader in=new BufferedReader(new InputStreamReader(urlConn.getInputStream()));
 		
 		int len;
+		String line="";
 		String str="";
-		byte[] buffer = new byte[4096];
 		PrintWriter out=response.getWriter();
-		while ((len = in.read(buffer)) > 0) {
-			
-			String s=new String(buffer);
-			buffer = new byte[4096];
-			str=str+s;
+		for (line = in.readLine(); line != null; line = in.readLine()) {
+		    str=str+line;
 		}
 		System.out.println(str);
 		out.println(str);
@@ -43,6 +44,6 @@ public class ProxyServlet extends HttpServlet {
     }
 	protected void doGet(HttpServletRequest request ,HttpServletResponse response) throws ServletException,IOException
     {
-		
+		doPost(request,response);
     }
 }
