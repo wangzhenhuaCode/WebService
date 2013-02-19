6560c2df99118dbe8ee7d8a47cde0d2f69b78898
yue ma <yuema@andrew.cmu.edu>
2013-02-19 14:49:25 -0500

    a

--- WebService/src/ajaxProxy/YelpServlet.java
+++ WebService/src/ajaxProxy/YelpServlet.java
@@ -10,6 +10,10 @@ import javax.servlet.http.*;
 import JSON.Bean.Business;
 import JSON.Bean.Score;
 import JSON.Bean.YelpBean;
+import JSON.Bean.Twitter.MoodType;
+import JSON.Bean.Twitter.Result;
+import JSON.Bean.Twitter.TwitterBean;
+import MachineLearning.NBTest;
 
 public class YelpServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request,
@@ -82,6 +86,40 @@ public class YelpServlet extends HttpServlet {
 		score.finalScore=((double)(competition+saturation))/2;
 		String str = JSONUtils.toJson(score);
 		out.println(str);
+		
+		consumerKey = "KkWZJyZ9LMVFNNt6pmxoDg";
+		consumerSecret = "uWI0MvkJ0wHqCEF8dsVR0yGPBRjdCJvcpAJv0M7gg";
+		token = "12332042-Ql4ryiwsIItryn9WZrlJD0lPUaaDES5ib1VYLOJrg";
+		tokenSecret = "wgNsRajf6HYm1oyHCxy9mF3cx1PuEMLhhIJTAf4Cgq8";
+
+	    Twitter twitter = new Twitter(consumerKey, consumerSecret, token, tokenSecret);
+	    latitude = Double.parseDouble(request.getParameter("latitude"));
+		longitude = Double
+				.parseDouble(request.getParameter("longitude"));
+	    String twitterJsonString = twitter.search(latitude, longitude);
+	    System.out.println(twitterJsonString);
+	    TwitterBean twitterBean = JSONUtils.fromJson(twitterJsonString, TwitterBean.class);
+		Result[] results = twitterBean.getResults();
+		String testTweets = "";
+		for(Result result : results) {
+			testTweets += result.getText().substring(8)+"~!@##@!~";
+		}
+		String temp = null;
+		try {
+			temp = NBTest.test(testTweets, "trainData.txt");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.print(temp);
+		String[] temps = temp.split(",");
+		MoodType type1 =new MoodType();
+		type1.good = Integer.parseInt(temps[1]);
+		type1.bad = Integer.parseInt(temps[3]);
+		type1.prob = type1.good*1.0/type1.bad;
+		String str1 = JSONUtils.toJson(type);
+
+		out.println(str1);
 		out.flush();
 		out.close();
 
